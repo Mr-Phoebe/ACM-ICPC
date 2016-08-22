70a017fee27142e0f4d1b171bc20982a3f8d31c3
HaonanWu <whn289467822@outlook.com>
2016-08-23 06:58:30 +0800

    leetcode 387

---
+++ OJ/Leetcode/Algorithm/387. First Unique Character in a String.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return min([s.find(c) for c in string.ascii_lowercase if s.count(c)==1] or [-1])
