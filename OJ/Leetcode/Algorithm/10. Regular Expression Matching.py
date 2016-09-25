c54a20700eee193e5aec41d8a34c5e7e212deba3
HaonanWu <whn289467822@outlook.com>
2016-09-25 11:43:24 +0800

    Leetcode 10

---
+++ OJ/Leetcode/Algorithm/10. Regular Expression Matching.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        return re.match('^' + p + '$', s) != None
