985ef0e75b50d4358fa97d5664adc1e28fbd05b4
HaonanWu <whn289467822@outlook.com>
2016-10-31 11:28:48 +0800

    leetcode 43

---
+++ OJ/Leetcode/Algorithm/43. Multiply Strings.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num1 = num1[::-1]; num2 = num2[::-1]
+        arr = [0 for i in range(len(num1)+len(num2))]
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                arr[i+j] += int(num1[i]) * int(num2[j])
+        ans = []
+        for i in range(len(arr)):
+            digit = arr[i] % 10
+            carry = arr[i] / 10
+            if i < len(arr)-1:
+                arr[i+1] += carry
+            ans.insert(0, str(digit))
+        while ans[0] == '0' and len(ans) > 1:
+            del ans[0]
+        return ''.join(ans)
