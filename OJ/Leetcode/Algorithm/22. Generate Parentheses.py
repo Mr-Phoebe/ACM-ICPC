939a06d1ba105728261c927cee0cd5886fee4c9b
HaonanWu <whn289467822@outlook.com>
2016-12-02 19:06:53 +0800

    Leetcode 22

---
+++ OJ/Leetcode/Algorithm/22. Generate Parentheses.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        def generate(p, left, right, parens=[]):
+            if left:         generate(p + '(', left-1, right)
+            if right > left: generate(p + ')', left, right-1)
+            if not right:    parens += p,
+            return parens
+        return generate('', n, n)
