00a329cb590ae6fed24cebcdf3d12c31ede731f5
HaonanWu <whn289467822@outlook.com>
2016-12-02 19:07:10 +0800

    Leetcode 27

---
+++ OJ/Leetcode/Algorithm/27. Remove Element.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            if nums[start] == val:
+                nums[start], nums[end], end = nums[end], nums[start], end - 1
+            else:
+                start +=1
+        return start
