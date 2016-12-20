4c99a1bd4cb78a26b49949e0bf8a116873cda8d3
HaonanWu <whn289467822@outlook.com>
2016-12-20 11:04:11 +0800

    Leetcode 25

---
+++ OJ/Leetcode/Algorithm/25. Reverse Nodes in k-Group.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        dummy = jump = ListNode(0)
+        dummy.next = l = r = head
+        
+        while True:
+            count = 0
+            while r and count < k:
+                r = r.next
+                count += 1
+            if count == k:
+                pre, cur = r, l
+                for _ in range(k):
+                    cur.next, cur, pre = pre, cur.next, cur
+                jump.next, jump, l = pre, l, r
+            else:
+                return dummy.next
