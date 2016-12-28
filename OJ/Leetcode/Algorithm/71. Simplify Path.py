a598f975890895fb7b6614555c1fbaf351ee7cd9
HaonanWu <whn289467822@outlook.com>
2016-12-28 19:19:58 +0800

    leetcode 71

---
+++ OJ/Leetcode/Algorithm/71. Simplify Path.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        places = [p for p in path.split("/") if p!="." and p!=""]
+        stack = []
+        for p in places:
+            if p == "..":
+                if len(stack) > 0:
+                    stack.pop()
+            else:
+                stack.append(p)
+        return "/" + "/".join(stack)
+        
