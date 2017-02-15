f22e64242e6604243a0bbaea2d8b9b01e56cd97c
HaonanWu <whn289467822@outlook.com>
2017-02-15 10:52:25 +0800

    leetcode 68

---
+++ OJ/Leetcode/Algorithm/68. Text Justification.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def fullJustify(self, words, maxWidth):
+        """
+        :type words: List[str]
+        :type maxWidth: int
+        :rtype: List[str]
+        """
+        res, cur, num_of_letters = [], [], 0
+        for w in words:
+            if num_of_letters + len(w) + len(cur) > maxWidth:
+                for i in range(maxWidth - num_of_letters):
+                    cur[i%(len(cur)-1 or 1)] += ' '
+                res.append(''.join(cur))
+                cur, num_of_letters = [], 0
+            cur += [w]
+            num_of_letters += len(w)
+        return res + [' '.join(cur).ljust(maxWidth)]
