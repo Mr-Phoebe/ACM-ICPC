bc6147801fb80ff6703bbd9a3b42faef971d20e8
HaonanWu <whn289467822@outlook.com>
2017-02-16 21:35:31 +0800

    104

---
+++ OJ/Leetcode/Algorithm/101. Symmetric Tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def isSym(L,R):
+            if not L and not R: return True
+            if L and R and L.val == R.val: 
+                return isSym(L.left, R.right) and isSym(L.right, R.left)
+            return False
+        return isSym(root, root)
