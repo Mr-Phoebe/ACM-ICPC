47a75a0f0fc31a4b16d8faba7c08aa687619dfe8
HaonanWu <whn289467822@outlook.com>
2018-03-03 10:51:08 -0500

    leetcode 598

--- OJ/Leetcode/Algorithm/598. Range Addition II.java
+++ OJ/Leetcode/Algorithm/598. Range Addition II.java
@@ -1,13 +1,15 @@
-public int maxCount(int m, int n, int[][] ops) {  
-    // 初始化为m,n可以省略if，否则不可省  
-    int row = m;  
-    int col = n;  
-    for (int[] op : ops) {  
-        // op[0]是横坐标  
-        row = Math.min(row, op[0]);  
-        // op[1]是纵坐标  
-        col = Math.min(col, op[1]);  
+class Solution {
+    public int maxCount(int m, int n, int[][] ops) {  
+        // 初始化为m,n可以省略if，否则不可省  
+        int row = m;  
+        int col = n;  
+        for (int[] op : ops) {  
+            // op[0]是横坐标  
+            row = Math.min(row, op[0]);  
+            // op[1]是纵坐标  
+            col = Math.min(col, op[1]);  
+        }  
+        // row * col是左上角的元素个数  
+        return row * col;  
     }  
-    // row * col是左上角的元素个数  
-    return row * col;  
-}  
+}
