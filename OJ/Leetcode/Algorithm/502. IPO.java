4215dfada7eef1e2e8753d4d63fe73cc87dee944
Haonan Wu <whn289467822@outlook.com>
2018-08-04 21:00:33 -0700

    leetcode

--- OJ/Leetcode/Algorithm/502. IPO.java
+++ OJ/Leetcode/Algorithm/502. IPO.java
@@ -1,22 +1,23 @@
 class Solution {
     public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
-        PriorityQueue<int[]> q1 = new PriorityQueue<>((a, b) -> (a[0] - a[1]));
-        PriorityQueue<int[]> q2 = new PriorityQueue<>((a, b) -> (b[1] - b[0]));
+        PriorityQueue<int[]> q1 = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+        PriorityQueue<int[]> q2 = new PriorityQueue<>((a, b) -> (b[1] - a[1]));
 
-        for(int i = 0; i < Profits.length; i++) {
-            q2.add(new int[] {Capital[i], Profits[i]});
+        for(int i = 0; i < Profits.length; i++)
+        {
+            q1.add(new int[] {Capital[i], Profits[i]});
         }
 
         for(int i = 0; i < k; i++)
         {
-            while(!q2.isEmpty() && q2.peek()[0] <= W) {
-                q1.add(q2.poll());
+            while(!q1.isEmpty() && q1.peek()[0] <= W) {
+                q2.add(q1.poll());
             }
 
-            if(q1.isEmpty())
+            if(q2.isEmpty())
                 break;
 
-            W += q1.poll()[1];
+            W += q2.poll()[1];
         }
 
         return W;
